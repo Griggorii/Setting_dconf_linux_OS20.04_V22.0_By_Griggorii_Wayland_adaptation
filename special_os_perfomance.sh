476d5835e5276ea49868c6763ef042b8fa7656df
Griggorii <40373174+Griggorii@users.noreply.github.com>
2020-11-24 00:51:30 +0800

    Add files via upload

---
+++ special_os_perfomance.sh
@@ -0,0 +1,84 @@
+#!/bin/bash
+
+####Griggorii@gmail.com mit license special os perfomance
+
+sudo ln -s /usr/lib/x86_64-linux-gnu/libcuda.so /usr/lib64/libcuda.so
+EOF
+sudo rm /usr/bin/gcc && sudo ln -s /usr/bin/gcc-10 /usr/bin/gcc
+EOF
+sudo rm /usr/bin/gcc-ar && sudo ln -s /usr/bin/gcc-ar-10 /usr/bin/gcc-ar
+EOF
+sudo rm /usr/bin/gcc-ranlib && sudo ln -s /usr/bin/gcc-ranlib-10 /usr/bin/gcc-ranlib
+EOF
+sudo rm /usr/bin/g++ && sudo ln -s /usr/bin/g++-10 /usr/bin/g++
+EOF
+sudo rm /usr/bin/gcc-ar && sudo ln -s /usr/bin/gcc-ar-10 /usr/bin/gcc-ar
+EOF
+sudo rm /usr/bin/gcc-ranlib && sudo ln -s /usr/bin/gcc-ranlib-10 /usr/bin/gcc-ranlib
+EOF
+sudo rm /usr/bin/x86_64-linux-gnu-gcc && sudo ln -s /usr/bin/x86_64-linux-gnu-gcc-10 /usr/bin/x86_64-linux-gnu-gcc
+EOF
+sudo rm /usr/bin/x86_64-linux-gnu-gcc-ar && sudo ln -s /usr/bin/x86_64-linux-gnu-gcc-ar-10 /usr/bin/x86_64-linux-gnu-gcc-ar
+EOF
+sudo rm /usr/bin/x86_64-linux-gnu-gcc-nm && sudo ln -s /usr/bin/x86_64-linux-gnu-gcc-nm-10 /usr/bin/x86_64-linux-gnu-gcc-nm
+EOF
+sudo rm /usr/bin/x86_64-linux-gnu-gcc-ranlib && sudo ln -s /usr/bin/x86_64-linux-gnu-gcc-ranlib-10 /usr/bin/x86_64-linux-gnu-gcc-ranlib
+EOF
+sudo ln -s /usr/bin/x86_64-linux-gnu-x86_64-linux-gnu-gcc-10 /usr/bin/x86_64-linux-gnu-gcc
+EOF
+sudo ln -s /usr/bin/x86_64-linux-gnu-x86_64-linux-gnu-gcc-ar-10 /usr/bin/x86_64-linux-gnu-gcc-ar
+EOF
+sudo ln -s /usr/bin/x86_64-linux-gnu-x86_64-linux-gnu-gcc-nm-10 /usr/bin/x86_64-linux-gnu-gcc-nm
+EOF
+sudo ln -s /usr/bin/x86_64-linux-gnu-x86_64-linux-gnu-gcc-ranlib-10 /usr/bin/x86_64-linux-gnu-gcc-ranlib
+EOF
+sudo ln -s /usr/include/python3.8/* /usr/include/*
+EOF
+sudo rm /usr/bin/python3 && sudo ln -s /usr/bin/python3.8 /usr/bin/python3
+EOF
+sudo ln -s /usr/bin/python3.9 /usr/bin/python3
+EOF
+sudo ln -s /usr/bin/python3.7 /usr/bin/python3
+EOF
+sudo ln -s /usr/bin/python3.6 /usr/bin/python3
+EOF
+sudo ln -s /usr/bin/python3.5 /usr/bin/python3
+EOF
+sudo ln -s /usr/bin/python2.7 /usr/bin/python2
+EOF
+sudo ln -s /usr/bin/python2 /usr/bin/python
+EOF
+sudo ln -s /usr/lib/x86_64-linux-gnu/libgio-2.0.so.0 /usr/lib/x86_64-linux-gnu/libgio-2.0.so
+EOF
+sudo ln -s /usr/lib/x86_64-linux-gnu/libgobject-2.0.so.0 /usr/lib/x86_64-linux-gnu/libgobject-2.0.so
+EOF
+sudo ln -s /usr/lib/x86_64-linux-gnu/libglib-2.0.so.0 /usr/lib/x86_64-linux-gnu/libglib-2.0.so
+EOF
+sudo ln -s /usr/lib/x86_64-linux-gnu/libGL.so.1.7.0 /usr/lib/x86_64-linux-gnu/libGL.so 
+EOF
+sudo ln -s /usr/lib/x86_64-linux-gnu/libGL.so.1.7.0 /usr/lib/x86_64-linux-gnu/libGL.so.1
+EOF
+sudo ln -s /usr/lib/x86_64-linux-gnu/libICE.so.6.3.0 /usr/lib/x86_64-linux-gnu/libICE.so
+EOF
+sudo ln -s /usr/lib/x86_64-linux-gnu/libICE.so.6.3.0 /usr/lib/x86_64-linux-gnu/libICE.so.6
+EOF
+sudo ln -s /usr/lib/x86_64-linux-gnu/libSM.so.6.0.1 /usr/lib/x86_64-linux-gnu/libSM.so
+EOF
+sudo ln -s /usr/lib/x86_64-linux-gnu/libSM.so.6.0.1 /usr/lib/x86_64-linux-gnu/libSM.so.6
+EOF
+sudo ln -s /usr/lib/x86_64-linux-gnu/libGL.so.1 /usr/lib/x86_64-linux-gnu/libGL.so
+EOF
+sudo ln -s /usr/lib/x86_64-linux-gnu/libicui18n.so.66 /usr/lib/x86_64-linux-gnu/libicui18n.so
+EOF
+sudo ln -s /usr/lib/x86_64-linux-gnu/libicuuc.so.66 /usr/lib/x86_64-linux-gnu/libicuuc.so
+EOF
+sudo ln -s /usr/lib/x86_64-linux-gnu/libicudata.so.66 /usr/lib/x86_64-linux-gnu/libicudata.so
+EOF
+
+
+
+
+
+
+
+
